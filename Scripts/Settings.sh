6476d0b306621bc2de2e76537a222c73d418be65
urock666 <37131443+urock666@users.noreply.github.com>
2024-02-28 15:46:18 +0800

    Update Settings.sh

--- Scripts/Settings.sh
+++ Scripts/Settings.sh
@@ -38,5 +38,6 @@ if [[ $WRT_URL == *"lede"* ]]; then
 elif [[ $WRT_URL == *"immortalwrt"* ]]; then
 	echo "CONFIG_PACKAGE_luci=y" >> ./.config
 	echo "CONFIG_LUCI_LANG_zh_Hans=y" >> ./.config
+ 	echo "CONFIG_PACKAGE_luci-app-homeproxy=y" >> ./.config
  	echo "CONFIG_PACKAGE_luci-app-openclash=y" >> ./.config
 fi
